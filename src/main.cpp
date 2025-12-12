f0b1c9d4e2c88226204d760d3b688441547296c7
kapt0xa <kuzmich.ml@gmail.com>
2025-12-12 12:46:19 +0300

    GLFW ununtu sucsessful test launch

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ int GlfwTestCode()
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "Das verde (werde) Spiel!", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
