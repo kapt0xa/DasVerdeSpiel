0e0f7e52968d6193d9aad5d8085627522ba49d2d
kapt0xa <kuzmich.ml@gmail.com>
2025-12-15 22:24:38 +0300

    test code worked but bad styte

--- src/main.cpp
+++ src/main.cpp
@@ -33,6 +33,15 @@ int GlfwTestCode()
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glBegin(GL_TRIANGLES);
+            glColor3f(1.0f, 0.0f, 0.0f); // Red
+            glVertex2f(-0.5f, -0.5f);
+            glColor3f(0.0f, 1.0f, 0.0f); // Green
+            glVertex2f(0.5f, -0.5f);
+            glColor3f(0.0f, 0.0f, 1.0f); // Blue
+            glVertex2f(0.0f, 0.5f);
+        glEnd();
+
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
