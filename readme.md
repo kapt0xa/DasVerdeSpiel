a7298b2b7ca72594b1e8723835f3047a5d2f39a4
kapt0xa <kuzmich.ml@gmail.com>
2025-12-21 10:12:07 +0300

    glewinit problems

--- readme.md
+++ readme.md
@@ -4,6 +4,7 @@
 OpenGL for 3d-graphics
 tutorial:
 https://www.youtube.com/watch?v=OR4fNpBjmq8&list=PLlrATfBNZ98foTJPJ_Ev03o2oq3-GGOS2&index=2
+documentation: docs.GL
 
 Bullet Physics or NVIDIA PhysX for physics
 ```
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include <iostream>
+
 // https://www.glfw.org/documentation.html
+// but i edit it
 int GlfwTestCode();
 
 int main()
@@ -14,20 +17,42 @@ int GlfwTestCode()
     GLFWwindow* window;
 
     /* Initialize the library */
-    if (!glfwInit()) return -1;
-    
-    glewInit();
+    if (!glfwInit())
+    {
+        std::cerr << "Error initializing GLFW!" << std::endl;
+        return -1;
+    }
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Das verde (werde) Spiel!", NULL, NULL);
     if (!window)
     {
+        std::cerr << "Error creating GLFW window!" << std::endl;
         glfwTerminate();
         return -1;
     }
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    if(glewInit() != GLEW_OK)
+    {
+        std::cerr << "Error initializing GLEW!" << std::endl;
+        return -1;
+    }
+
+    std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
+
+    // float positions[6] ={
+    //     -0.5f, -0.5f,
+    //      0.5f, -0.5f,
+    //      0.0f,  0.5f,
+    // };
+
+    // GLuint buffer;
+    // glGenBuffers(1, &buffer);
+    // glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    // glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
