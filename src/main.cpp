82ccc1e8f2ff6b876fc7a36047c365d6e0b5fbaf
kapt0xa <kuzmich.ml@gmail.com>
2025-12-21 21:23:51 +0300

    cpp standart

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
 #include <GLFW/glfw3.h>
+#include <string>
+#include <iostream>
+#include <functional>
 
 // https://www.glfw.org/documentation.html
 int GlfwTestCode();
@@ -53,3 +56,42 @@ int GlfwTestCode()
 
     return 0;
 }
+
+class GLFW
+{
+private:
+    GLFWwindow* window;
+    int width;
+    int height;
+    std::string title;
+public:
+    inline GLFW(int w = 854, int h = 480, const std::string& t = "GLFW Window")
+        : width(w), height(h), title(t)
+    {
+        if (!glfwInit())
+        {
+            std::cerr << "Failed to initialize GLFW" << std::endl;
+            throw std::runtime_error("GLFW Initialization Failed");
+        }
+
+        window = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
+        if (!window)
+        {
+            glfwTerminate();
+            std::cerr << "Failed to create GLFW window" << std::endl;
+            throw std::runtime_error("GLFW Window Creation Failed");
+        }
+
+        glfwMakeContextCurrent(window);
+    }
+    inline ~GLFW()
+    {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+
+    GLFW(const GLFW&) = delete;
+    GLFW& operator=(const GLFW&) = delete;
+    GLFW(GLFW&&) = delete;
+    GLFW& operator=(GLFW&&) = delete;
+};
